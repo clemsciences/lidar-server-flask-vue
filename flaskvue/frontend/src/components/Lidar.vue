d22066d557c09db16189507cf5ab75665a4eaef9
clement_besnier <clemsciences@aol.com>
2019-06-12 18:15:28 +0200

    Added Plotly

--- flaskvue/frontend/src/components/Lidar.vue
+++ flaskvue/frontend/src/components/Lidar.vue
@@ -1,8 +1,14 @@
 <template>
-    
+    <div id="lidar-view">
+
+    </div>
+
 </template>
 
 <script>
+
+    import Plotly from 'plotly.js-dist';
+    Plotly.
     export default {
         name: "Lidar"
     }
