c1933d11d0550e1baf5a9a6c181cd91ae237a112
clement_besnier <clemsciences@aol.com>
2019-07-26 17:30:55 +0200

    Added a wrapper of Plotly for VUE JS

--- flaskvue/frontend/src/components/Lidar.vue
+++ flaskvue/frontend/src/components/Lidar.vue
@@ -1,18 +1,46 @@
 <template>
 
     <v-container id="lidar-view">
+        <Plotly :data="data" :layout="layout"/>
 
     </v-container>
 
 </template>
 
 <script>
+    import { Plotly } from 'vue-plotly';
 
-    import Plotly from 'plotly.js-dist';
-    let a = Plotly();
     export default {
-        name: "Lidar"
-    }
+
+        name: "Lidar",
+
+        components: {
+            Plotly
+        },
+
+        data() {
+            return {
+                data: [{
+                    x: [1, 2, 3, 4, 5],
+                    y: [1, 6, 3, 6, 1],
+                    mode: 'markers',
+                    type: 'scatter',
+                    name: 'bléré',
+                    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+                    marker: {size: 12}
+                },],
+                layout: {
+                    xaxis: {
+                        range: [0.75, 5.25]
+                    },
+                    yaxis: {range: [-8, 8]},
+                    title: 'Le titre'
+                }
+            }
+        }
+    };
+
+
 </script>
 
 <style scoped>
