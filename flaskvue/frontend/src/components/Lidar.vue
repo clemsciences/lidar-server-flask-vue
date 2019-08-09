b6e97481a9a44270d857678e8786a198b85e8571
clemsciences <clemsciences@aol.com>
2019-08-09 23:56:06 +0200

    Example of polar chart

--- flaskvue/frontend/src/components/Lidar.vue
+++ flaskvue/frontend/src/components/Lidar.vue
@@ -10,6 +10,9 @@
 <script>
     import { Plotly } from 'vue-plotly';
 
+
+
+
     export default {
 
         name: "Lidar",
@@ -21,20 +24,27 @@
         data() {
             return {
                 data: [{
-                    x: [1, 2, 3, 4, 5],
-                    y: [1, 6, 3, 6, 1],
-                    mode: 'markers',
-                    type: 'scatter',
-                    name: 'bléré',
-                    text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-                    marker: {size: 12}
+                    r: [1000, 1200, 1700, 750, 2400, 2000],
+                    theta: [0, 0.2*180/3.14, 180/3.14, 2*180/3.14, 3*180/3.14, 5*180/3.14],
+                    mode: 'lines',
+                    name: 'lidar_simulation',
+                    line: {color: 'red'},
+                    type: 'scatterpolar',
+
+                    // x: [1, 2, 3, 4, 5],
+                    // y: [1, 6, 3, 6, 1],
+                    // mode: 'markers',
+                    // type: 'scatter',
+                    // name: 'bléré',
+                    // text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
+                    // marker: {size: 12}
                 },],
                 layout: {
                     xaxis: {
                         range: [0.75, 5.25]
                     },
                     yaxis: {range: [-8, 8]},
-                    title: 'Le titre'
+                    title: 'Simulation mesures LiDAR'
                 }
             }
         }
